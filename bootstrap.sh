015f45aa686200ec5fdf6b11cf08f51c109287bd
Kot <kot@yukata.tech>
2022-05-09 13:51:36 -0700

    yes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,2 @@
 wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb && sudo dpkg -i erlang-solutions_2.0_all.deb
-sudo apt-get update && sudo apt-get install esl-erlang elixir
+sudo apt-get update && sudo apt-get install -yq esl-erlang elixir
