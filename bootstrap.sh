e031e3ca1bdb04401ab0630541bcc2bc42c8b877
Kot <kot@yukata.tech>
2022-06-11 11:44:51 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,6 @@
+# Elixir/Erlang
 wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb && sudo dpkg -i erlang-solutions_2.0_all.deb
 sudo apt-get update && sudo apt-get install -yq esl-erlang elixir
+
+# Fly.io
+curl -L https://fly.io/install.sh | sh
